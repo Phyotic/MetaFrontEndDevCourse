36a06cba4c03a536f72ad937e8bc11a11801e7f7
Daniel Solorzano <dsolo.prof@gmail.com>
2024-09-13 14:17:52 -0500

    Added semantic html and OGP.

---
+++ Course8/little-lemon/src/Components/Header.js
@@ -0,0 +1,11 @@
+import logo from "../images/Logo.svg";
+
+function Header() {
+    return (
+        <header>
+            <img src={logo} alt="Little Lemon Logo"></img>
+        </header>
+    );
+}
+
+export default Header;
