3d12922390cecc5b3923ea4c7f4c4836a3d06929
Daniel Solorzano <dsolo.prof@gmail.com>
2024-10-04 14:48:19 -0500

    Created ConfirmedBooking component startup.

---
+++ Course8/little-lemon/src/Components/ConfirmedBooking/ConfirmedBooking.jsx
@@ -0,0 +1,11 @@
+import "./ConfirmedBooking.css";
+
+function ConfirmedBooking() {
+    return (
+        <>
+            <h1>Booking has been confirmed</h1>
+        </>
+    );
+}
+
+export default ConfirmedBooking;
