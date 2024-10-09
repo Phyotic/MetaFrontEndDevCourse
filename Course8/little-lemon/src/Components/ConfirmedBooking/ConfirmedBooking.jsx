c41e0cfeee87853fdd5306ab4b392a90d1c3716d
Daniel Solorzano <dsolo.prof@gmail.com>
2024-10-09 00:08:15 -0500

    Updated ConfirmedBooking component CSS.

--- Course8/little-lemon/src/Components/ConfirmedBooking/ConfirmedBooking.jsx
+++ Course8/little-lemon/src/Components/ConfirmedBooking/ConfirmedBooking.jsx
@@ -3,7 +3,9 @@ import "./ConfirmedBooking.css";
 function ConfirmedBooking() {
     return (
         <>
-            <h1>Booking has been confirmed</h1>
+            <h1 id="confirm-text" className="markazi-text-medium">
+                Booking has been confirmed
+            </h1>
         </>
     );
 }
