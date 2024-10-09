c61c6ebeff2592bfe5be57dd14e83749f294f11c
Daniel Solorzano <dsolo.prof@gmail.com>
2024-10-09 00:07:18 -0500

    Completed DishCard component CSS.

--- Course8/little-lemon/src/Components/DishCard/DishCard.jsx
+++ Course8/little-lemon/src/Components/DishCard/DishCard.jsx
@@ -7,17 +7,19 @@ function DishCard({ name, price, info, imageUrl }) {
             <img src={imageUrl} width="100%" alt={name}></img>
 
             <section className="dish-info">
-                <section className="dish-name-price">
+                <section className="dish-name-price markazi-text-medium">
                     <p>{name}</p>
                     <p>
                         <span className="highlight-orange">${price}</span>
                     </p>
                 </section>
 
-                <p>{info}</p>
+                <p className="karla-regular">{info}</p>
 
                 <section className="card-deliver">
-                    <button>Order a delivery</button>
+                    <button className="deliver-button markazi-text-medium">
+                        Order a delivery
+                    </button>
                     <img
                         className="delivery-bike"
                         alt="delivery bike icon"
